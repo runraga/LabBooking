5ec1e5b089abba7f2ebfb3df07adb576c22037a1
James Ault <james@helmfarm.com>
2024-07-30 15:02:47 +0100

    implement interface for booking objects

---
+++ LabBookingLib/Models/Booking.cs
@@ -0,0 +1,28 @@
+namespace LabBookingLib.Models;
+
+
+
+public interface IBooking
+{
+    void AddDataFile(IDatafile rawDataFile);
+    DateTime GetBookingStartTime();
+    int BookingLengthMinutes { get; }
+    bool IsValidBooking { get; }
+    bool IsWithinBookingWindow(IDatafile otherFile);
+    bool BookingComplete { get; set; }
+    //string BookingError { get; }
+    Task<bool> MakeBookingAsync();
+    string ProjectCode { get; }
+
+    //Dictionary<string, List<string>> GetDataFileErrors();
+    List<string> GetDataFileNames();
+    public IInstrument Instrument { get; }
+
+}
+public class BookingException : Exception
+{
+    public BookingException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
